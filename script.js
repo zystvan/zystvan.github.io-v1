eb1cae6ac77ba3e8ab9798883df270a0e9307e87
Zeke Y <ezekiel@yarashus.com>
2015-01-15 17:01:50 -0500

    Removed an image and added some comments

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   // get a random number between 1 and 20
   var min = 1;
-  var max = 20;
+  var max = 18;
   var random = Math.floor(Math.random() * (max - min + 1)) + min;
   
   // generate a background image based on the random number
@@ -16,52 +16,51 @@ $(document).ready(function() {
       $('header .background').css("background", "url('img/grass.jpg') center center no-repeat fixed");
       break;
     case 4:
-      $('header .background').css("background", "url('img/hillside.jpg') center center no-repeat fixed");
-      break;
-    case 5:
       $('header .background').css("background", "url('img/moose.jpg') center center no-repeat fixed");
       break;
-    case 6:
+    case 5:
       $('header .background').css("background", "url('img/mountain.jpg') center center no-repeat fixed");
       break;
-    case 7:
+    case 6:
       $('header .background').css("background", "url('img/mountain-top.jpg') center center no-repeat fixed");
       break;
-    case 8:
+    case 7:
       $('header .background').css("background", "url('img/ocean-mountains.jpg') center center no-repeat fixed");
       break;
-    case 9:
+    case 8:
       $('header .background').css("background", "url('img/stream.jpg') center center no-repeat fixed");
       break;
-    case 10:
+    case 9:
       $('header .background').css("background", "url('img/volcano.jpg') center center no-repeat fixed");
       break;
-    case 11:
+    case 10:
       $('header .background').css("background", "url('img/bear.jpg') center center no-repeat fixed");
       break;
-    case 12:
+    case 11:
       $('header .background').css("background", "url('img/leopard.jpg') center center no-repeat fixed");
       break;
-    case 13:
+    case 12:
       $('header .background').css("background", "url('img/mountain-ridge.jpg') center center no-repeat fixed");
       break;
-    case 14:
+    case 13:
       $('header .background').css("background", "url('img/sheep.jpg') center center no-repeat fixed");
       break;
-    case 16:
+    case 14:
       $('header .background').css("background", "url('img/snowy-mountains-evening.jpg') center center no-repeat fixed");
       break;
-    case 17:
+    case 15:
       $('header .background').css("background", "url('img/snowy-mountains-night.jpg') center center no-repeat fixed");
       break;
-    case 18:
+    case 16:
       $('header .background').css("background", "url('img/snowy-trees.jpg') center center no-repeat fixed");
       break;
-    case 19:
+    case 17:
       $('header .background').css("background", "url('img/watery-rocks.jpg') center center no-repeat fixed");
       break;
-    case 20:
+    case 18:
       $('header .background').css("background", "url('img/grass-mountain.jpg') center center no-repeat fixed");
       break;
   }
-  });
+});
+
+console.log("You don\'t have to be here! I made the code available on Github: \n https://github.com/KK4OXJ/KK4OXJ.github.io");
