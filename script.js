373b5ed89df3e99a10f418608eb3c159efd7ebc8
Zeke Y <ezekiel@yarashus.com>
2015-01-19 19:36:10 -0500

    Fixed the background not showing up

--- script.js
+++ script.js
@@ -8,116 +8,116 @@ $(document).ready(function() {
   if (window.location.pathname == "/" || window.location.pathname == "") {
     switch (random) {
       case 1:
-        $('header .background').css("background", "url('img/river-mountains-trees.jpg') center center no-repeat fixed");
+        $('.background').css("background", "url('img/river-mountains-trees.jpg') center center no-repeat fixed");
         break;
       case 2:
-        $('header .background').css("background", "url('img/fox.jpg') center center no-repeat fixed");
+        $('.background').css("background", "url('img/fox.jpg') center center no-repeat fixed");
         break;
       case 3:
-        $('header .background').css("background", "url('img/grass.jpg') center center no-repeat fixed");
+        $('.background').css("background", "url('img/grass.jpg') center center no-repeat fixed");
         break;
       case 4:
-        $('header .background').css("background", "url('img/moose.jpg') center center no-repeat fixed");
+        $('.background').css("background", "url('img/moose.jpg') center center no-repeat fixed");
         break;
       case 5:
-        $('header .background').css("background", "url('img/mountain.jpg') center center no-repeat fixed");
+        $('.background').css("background", "url('img/mountain.jpg') center center no-repeat fixed");
         break;
       case 6:
-        $('header .background').css("background", "url('img/mountain-top.jpg') center center no-repeat fixed");
+        $('.background').css("background", "url('img/mountain-top.jpg') center center no-repeat fixed");
         break;
       case 7:
-        $('header .background').css("background", "url('img/ocean-mountains.jpg') center center no-repeat fixed");
+        $('.background').css("background", "url('img/ocean-mountains.jpg') center center no-repeat fixed");
         break;
       case 8:
-        $('header .background').css("background", "url('img/stream.jpg') center center no-repeat fixed");
+        $('.background').css("background", "url('img/stream.jpg') center center no-repeat fixed");
         break;
       case 9:
-        $('header .background').css("background", "url('img/volcano.jpg') center center no-repeat fixed");
+        $('.background').css("background", "url('img/volcano.jpg') center center no-repeat fixed");
         break;
       case 10:
-        $('header .background').css("background", "url('img/bear.jpg') center center no-repeat fixed");
+        $('.background').css("background", "url('img/bear.jpg') center center no-repeat fixed");
         break;
       case 11:
-        $('header .background').css("background", "url('img/leopard.jpg') center center no-repeat fixed");
+        $('.background').css("background", "url('img/leopard.jpg') center center no-repeat fixed");
         break;
       case 12:
-        $('header .background').css("background", "url('img/mountain-ridge.jpg') center center no-repeat fixed");
+        $('.background').css("background", "url('img/mountain-ridge.jpg') center center no-repeat fixed");
         break;
       case 13:
-        $('header .background').css("background", "url('img/sheep.jpg') center center no-repeat fixed");
+        $('.background').css("background", "url('img/sheep.jpg') center center no-repeat fixed");
         break;
       case 14:
-        $('header .background').css("background", "url('img/snowy-mountains-evening.jpg') center center no-repeat fixed");
+        $('.background').css("background", "url('img/snowy-mountains-evening.jpg') center center no-repeat fixed");
         break;
       case 15:
-        $('header .background').css("background", "url('img/snowy-mountains-night.jpg') center center no-repeat fixed");
+        $('.background').css("background", "url('img/snowy-mountains-night.jpg') center center no-repeat fixed");
         break;
       case 16:
-        $('header .background').css("background", "url('img/snowy-trees.jpg') center center no-repeat fixed");
+        $('.background').css("background", "url('img/snowy-trees.jpg') center center no-repeat fixed");
         break;
       case 17:
-        $('header .background').css("background", "url('img/watery-rocks.jpg') center center no-repeat fixed");
+        $('.background').css("background", "url('img/watery-rocks.jpg') center center no-repeat fixed");
         break;
       case 18:
-        $('header .background').css("background", "url('img/grass-mountain.jpg') center center no-repeat fixed");
+        $('.background').css("background", "url('img/grass-mountain.jpg') center center no-repeat fixed");
         break;
     }
   }
   else if (window.location.pathname == "/portfolio/" || window.location.pathname == "/portfolio") {
     switch (random) {
       case 1:
-        $('header .background').css("background", "url('../img/river-mountains-trees.jpg') center center no-repeat fixed");
+        $('.background').css("background", "url('../img/river-mountains-trees.jpg') center center no-repeat fixed");
         break;
       case 2:
-        $('header .background').css("background", "url('../img/fox.jpg') center center no-repeat fixed");
+        $('.background').css("background", "url('../img/fox.jpg') center center no-repeat fixed");
         break;
       case 3:
-        $('header .background').css("background", "url('../img/grass.jpg') center center no-repeat fixed");
+        $('.background').css("background", "url('../img/grass.jpg') center center no-repeat fixed");
         break;
       case 4:
-        $('header .background').css("background", "url('../img/moose.jpg') center center no-repeat fixed");
+        $('.background').css("background", "url('../img/moose.jpg') center center no-repeat fixed");
         break;
       case 5:
-        $('header .background').css("background", "url('../img/mountain.jpg') center center no-repeat fixed");
+        $('.background').css("background", "url('../img/mountain.jpg') center center no-repeat fixed");
         break;
       case 6:
-        $('header .background').css("background", "url('../img/mountain-top.jpg') center center no-repeat fixed");
+        $('.background').css("background", "url('../img/mountain-top.jpg') center center no-repeat fixed");
         break;
       case 7:
-        $('header .background').css("background", "url('../img/ocean-mountains.jpg') center center no-repeat fixed");
+        $('.background').css("background", "url('../img/ocean-mountains.jpg') center center no-repeat fixed");
         break;
       case 8:
-        $('header .background').css("background", "url('../img/stream.jpg') center center no-repeat fixed");
+        $('.background').css("background", "url('../img/stream.jpg') center center no-repeat fixed");
         break;
       case 9:
-        $('header .background').css("background", "url('../img/volcano.jpg') center center no-repeat fixed");
+        $('.background').css("background", "url('../img/volcano.jpg') center center no-repeat fixed");
         break;
       case 10:
-        $('header .background').css("background", "url('../img/bear.jpg') center center no-repeat fixed");
+        $('.background').css("background", "url('../img/bear.jpg') center center no-repeat fixed");
         break;
       case 11:
-        $('header .background').css("background", "url('../img/leopard.jpg') center center no-repeat fixed");
+        $('.background').css("background", "url('../img/leopard.jpg') center center no-repeat fixed");
         break;
       case 12:
-        $('header .background').css("background", "url('../img/mountain-ridge.jpg') center center no-repeat fixed");
+        $('.background').css("background", "url('../img/mountain-ridge.jpg') center center no-repeat fixed");
         break;
       case 13:
-        $('header .background').css("background", "url('../img/sheep.jpg') center center no-repeat fixed");
+        $('.background').css("background", "url('../img/sheep.jpg') center center no-repeat fixed");
         break;
       case 14:
-        $('header .background').css("background", "url('../img/snowy-mountains-evening.jpg') center center no-repeat fixed");
+        $('.background').css("background", "url('../img/snowy-mountains-evening.jpg') center center no-repeat fixed");
         break;
       case 15:
-        $('header .background').css("background", "url('../img/snowy-mountains-night.jpg') center center no-repeat fixed");
+        $('.background').css("background", "url('../img/snowy-mountains-night.jpg') center center no-repeat fixed");
         break;
       case 16:
-        $('header .background').css("background", "url('../img/snowy-trees.jpg') center center no-repeat fixed");
+        $('.background').css("background", "url('../img/snowy-trees.jpg') center center no-repeat fixed");
         break;
       case 17:
-        $('header .background').css("background", "url('../img/watery-rocks.jpg') center center no-repeat fixed");
+        $('.background').css("background", "url('../img/watery-rocks.jpg') center center no-repeat fixed");
         break;
       case 18:
-        $('header .background').css("background", "url('../img/grass-mountain.jpg') center center no-repeat fixed");
+        $('.background').css("background", "url('../img/grass-mountain.jpg') center center no-repeat fixed");
         break;
     }
   }
