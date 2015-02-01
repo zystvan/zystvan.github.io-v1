3d089cb0621cbde306e1497e88c69dd859141642
Zeke Y <ezekiel@yarashus.com>
2015-01-31 22:37:33 -0500

    Inline some JS

--- js/script.js
+++ js/script.js
@@ -1,33 +1,4 @@
 $(document).ready(function() {
-  // array of all the background images
-  var bgImages = [
-    'img/river-mountains-trees.jpg',
-    'img/fox.jpg',
-    'img/grass.jpg',
-    'img/moose.jpg',
-    'img/mountain.jpg',
-    'img/mountain-top.jpg',
-    'img/ocean-mountains.jpg',
-    'img/stream.jpg',
-    'img/volcano.jpg',
-    'img/bear.jpg',
-    'img/leopard.jpg',
-    'img/mountain-ridge.jpg',
-    'img/watery-rocks.jpg',
-    'img/snowy-trees.jpg',
-    'img/snowy-mountains-night.jpg',
-    'img/snowy-mountains-evening.jpg',
-    'img/sheep.jpg',
-    'img/watery-rocks.jpg',
-    'img/grass-mountain.jpg'
-  ];
-
-  // randomly choose a background image...
-  var random = Math.floor(Math.random() * bgImages.length);
-  
-  // ...and set that background image
-  $('.background').css('background', 'url(\'' + bgImages[random] + '\') center center');
-  
   // smooth scroller widget thing by Chris Coyier (CSS-Tricks.com)
   $('a[href*=#]').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
