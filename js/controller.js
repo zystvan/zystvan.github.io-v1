37d041002c75584b40dee973427167bb71373730
Zeke Y <ezekiel@yarashus.com>
2015-02-06 18:11:38 +0000

    Update footer & portfolio section

--- js/controller.js
+++ js/controller.js
@@ -5,11 +5,13 @@ projects.controller('projectList', function($scope) {
     {
       'img': 'img/zeke-y-design.png',
       'url': '#',
+      'title': 'Zeke Y Design',
       'description': 'This website. Made with a mix of Javascript, jQuery, and Angular JS, I created it so I would have a public portfolio and a way for people to contact me.'
     },
     {
       'img': 'img/lnxreviews.png',
       'url': 'http://lnxreviews.com',
+      'title': 'LnxReviews',
       'description': 'LnxReviews is a project I worked on by myself, to improve my skills with WordPress, and to provide a single place for reliable, accurate, and up to date reviews of Linux distributions.'
     }
   ];
